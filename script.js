aa6ff926c9139f9746d97e9cf2e2e061016e26dd
vivekt007 <vivektigadi59@gmail.com>
2024-07-12 12:08:43 +0530

    "Updated CSS styles, added carousel functionality to index.html, and modified script.js to handle carousel slideshow. Also, updated trash.html with new carousel styles and script."

--- script.js
+++ script.js
@@ -19,3 +19,17 @@ function attendancePage() {
 function leavePage() {
     location.replace("leave.html")
 }
+
+let slides = document.querySelector(".slides");
+        let slideIndex = 0;
+
+        function showSlide(index) {
+            slides.style.transform = `translateX(-${index * 100}%)`;
+        }
+
+        function nextSlide() {
+            slideIndex = (slideIndex + 1) % slides.children.length;
+            showSlide(slideIndex);
+        }
+
+        setInterval(nextSlide, 2000); // Change slide every 3 seconds  
