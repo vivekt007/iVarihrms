c3038b24b141e2b7b11ead7024562f7f51ac12a2
vivekt007 <vivektigadi59@gmail.com>
2024-07-22 17:55:04 +0530

    sales updates and outreach

--- script.js
+++ script.js
@@ -12,6 +12,36 @@ window.onclick = function (e) {
     }
 }
 
+function attendancePage() {
+  location.replace("attendance.html")
+}
+
+function leavePage() {
+  location.replace("leave.html")
+}
+
+function uploadSales() {
+  location.replace("uploadSales.html")
+}
+
+function outReach() {
+  location.replace("outReach.html")
+}
+
+let slides = document.querySelector(".slides");
+let slideIndex = 0;
+
+function showSlide(index) {
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+  slideIndex = (slideIndex + 1) % slides.children.length;
+  showSlide(slideIndex);
+}
+
+setInterval(nextSlide, 2500); // Change slide every 3 seconds  
+
 function opentabs(evt, cityName) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
